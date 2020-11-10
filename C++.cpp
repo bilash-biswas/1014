abdabaab3a6976f32f985b1120b47aff222b204d
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-10 16:03:55 +0600

    Create  C++.cpp

---
+++ C++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a;
+    double b;
+    cin>>a;
+    cin>>b;
+    cout<<fixed<<setprecision(3)<<(a/b)<<" km/l"<<endl;
+    return 0;
+}
